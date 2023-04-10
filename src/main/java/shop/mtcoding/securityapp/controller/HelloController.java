e2160f9196d07d523668b7cdda0fa69e53b44dec
tero1115 <limjisang1115@gmail.com>
2023-04-10 10:07:49 +0900

    시큐리티 기본예제

---
+++ src/main/java/shop/mtcoding/securityapp/controller/HelloController.java
@@ -0,0 +1,19 @@
+package shop.mtcoding.securityapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class HelloController {
+   
+    @GetMapping("/")
+    public ResponseEntity<?> Hello(){
+        
+    return ResponseEntity.ok().body("ok");
+    }
+}
