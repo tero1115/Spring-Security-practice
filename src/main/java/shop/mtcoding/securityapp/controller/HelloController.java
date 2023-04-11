0a55351ae68bdf31c4c079d23908f431ca6c8d6c
tero1115 <limjisang1115@gmail.com>
2023-04-11 09:57:33 +0900

    시큐리티 SSR 로그인 처리

--- src/main/java/shop/mtcoding/securityapp/controller/HelloController.java
+++ src/main/java/shop/mtcoding/securityapp/controller/HelloController.java
@@ -1,12 +1,15 @@
 package shop.mtcoding.securityapp.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.securityapp.core.auth.MyUserDetails;
 import shop.mtcoding.securityapp.dto.ResponseDTO;
 import shop.mtcoding.securityapp.dto.UserRequest;
 import shop.mtcoding.securityapp.dto.UserResponse;
@@ -17,6 +20,15 @@ import shop.mtcoding.securityapp.service.UserService;
 public class HelloController {
 
     private final UserService userService;
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> userCheck(
+            @PathVariable Long id,
+            @AuthenticationPrincipal MyUserDetails myUserDetails){
+        String username = myUserDetails.getUser().getUsername();
+        String role = myUserDetails.getUser().getRole();
+        return ResponseEntity.ok().body(username + " : " + role);
+    }
 
     @GetMapping("/")
     public ResponseEntity<?> Hello() {
